d354af25a5cf29d9c72115f14279af22c4cf6c76
Nedal_Erekat <nedalerekat@gmail.com>
2020-10-12 20:07:50 +0300

    Updates

---
+++ precache-manifest.5ddc64507af5f121c7fb3760c7304d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b7fc313f0d1bebb4c91cf30ea2faaf",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "5039864c95445cf1d363",
+    "url": "/resty/static/css/main.f06ee8f9.chunk.css"
+  },
+  {
+    "revision": "737bc4cf80b854334010",
+    "url": "/resty/static/js/2.f2a65d02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.f2a65d02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5039864c95445cf1d363",
+    "url": "/resty/static/js/main.0bb1ed33.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
